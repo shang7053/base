ae0987f023b20ad77acfc83720f9cbc27d1a46d3
scc <javascc@126.com>
2015-11-16 19:53:21 +0800

    dubbo的demo添加注解方式
    dubbo的demo添加注解方式

--- dubbo-parent/dubbo-customer/src/main/java/scc/customer/service/impl/UserServiceImpl.java
+++ dubbo-parent/dubbo-customer/src/main/java/scc/customer/service/impl/UserServiceImpl.java
@@ -1,14 +1,16 @@
 package scc.customer.service.impl;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import scc.common.user.IUserDao;
 import scc.common.vo.User;
 import scc.customer.service.IUserService;
+
+import com.alibaba.dubbo.config.annotation.Reference;
 @Repository
 public class UserServiceImpl implements IUserService{
-	@Autowired
+	//@Autowired	xml配置时使用
+	@Reference
 	private IUserDao userDao;
 	public boolean addUser(User user) {
 		return userDao.addUser(user);
--- dubbo-parent/dubbo-provider/src/main/java/scc/provider/dao/impl/UserDaoImpl.java
+++ dubbo-parent/dubbo-provider/src/main/java/scc/provider/dao/impl/UserDaoImpl.java
@@ -4,9 +4,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
+import com.alibaba.dubbo.config.annotation.Service;
+
 import scc.common.user.IUserDao;
 import scc.common.vo.User;
-@Repository
+//@Repository	xml配置时使用
+@Service
 public class UserDaoImpl implements IUserDao{
 	@Autowired
 	private JdbcTemplate jdbcTemplate;

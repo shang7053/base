b755ab7546c06e4a041f1f762b28a38ac7cbea7b
scc <javascc@126.com>
2016-08-24 10:25:28 +0800

    test

--- stu-spring/src/main/java/test/controller/HomeController.java
+++ stu-spring/src/main/java/test/controller/HomeController.java
@@ -8,6 +8,7 @@ package test.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -19,39 +20,54 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import test.orm.ITestDao;
+import test.spel.SpelTest;
 
 /**
- * <p>Description: </p>
+ * <p>
+ * Description:
+ * </p>
+ * 
  * @author scc
  * @since 创建时间：2015年11月3日 上午10:18:15
  */
 @Controller
 public class HomeController {
-	@Value("#{testDao}")
-	private ITestDao iTestDao;
-	@RequestMapping("/home")
-	public String test1(Model model){
-		model.addAttribute("testVo", new TestVO());
-		return "index";
-	}
-	@RequestMapping("/add")
-	public String test2(@Valid @ModelAttribute("testVo") TestVO	testVo,BindingResult result){
-		if(result.hasErrors()) {
-			return "index";
-		} else {			
-			iTestDao.add(testVo);
-			return "index";
-		}
-	}
-	@RequestMapping("/list")
-	@ResponseBody
-	public List<String> getList(){
-		List<String> list=new ArrayList<String>();
-		list.add("哈哈");
-		list.add("哈哈");
-		list.add("哈哈");
-		list.add("哈哈");
-		list.add("哈哈");
-		return list;
-	}
+    @Value("#{testDao}")
+    private ITestDao iTestDao;
+    @Resource
+    private SpelTest spelTest;
+
+    @RequestMapping("/home")
+    public String test1(Model model) {
+        model.addAttribute("testVo", new TestVO());
+        return "index";
+    }
+
+    @RequestMapping("/add")
+    public String test2(@Valid @ModelAttribute("testVo") TestVO testVo, BindingResult result) {
+        if (result.hasErrors()) {
+            return "index";
+        } else {
+            this.iTestDao.add(testVo);
+            return "index";
+        }
+    }
+
+    @RequestMapping("/list")
+    @ResponseBody
+    public List<String> getList() {
+        List<String> list = new ArrayList<String>();
+        list.add("哈哈");
+        list.add("哈哈");
+        list.add("哈哈");
+        list.add("哈哈");
+        list.add("哈哈");
+        return list;
+    }
+
+    @RequestMapping("/testspel")
+    @ResponseBody
+    public String testspel() {
+        return this.spelTest.getflag();
+    }
 }

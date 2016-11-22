fd5a636ffb5a59248236d8aad1cfb524ddb7e909
scc <javascc@126.com>
2016-11-22 17:20:01 +0800

    hbase study

--- stu-hbase/src/main/java/scc/hbase/HbaseTest.java
+++ stu-hbase/src/main/java/scc/hbase/HbaseTest.java
@@ -9,6 +9,9 @@
 package scc.hbase;
 
 import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.Cell;
@@ -23,7 +26,11 @@ import org.apache.hadoop.hbase.client.HBaseAdmin;
 import org.apache.hadoop.hbase.client.HTable;
 import org.apache.hadoop.hbase.client.Put;
 import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
 import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.filter.PageFilter;
+import org.apache.hadoop.hbase.util.Bytes;
 
 /**
  *
@@ -37,7 +44,7 @@ public class HbaseTest {
     public Configuration configuration;
     public HTable hTable;
     public HBaseAdmin hBaseAdmin;
-    public Connection connection;
+    public ExecutorService executorService = Executors.newFixedThreadPool(5);
 
     /**
      *
@@ -52,25 +59,138 @@ public class HbaseTest {
      * @date 2016年11月21日 下午2:36:52
      */
     public HbaseTest() {
+
         this.configuration = HBaseConfiguration.create();
         this.configuration.set("hbase.master", "hadoop-n:60000");
         this.configuration.set("hbase.zookeeper.property.clientPort", "2181");
         this.configuration.set("hbase.zookeeper.quorum", "hadoop-n,hadoop-d1,hadoop-d2");
+    }
+
+    public Connection getConnection() {
+        Connection connection = null;
         try {
-            this.connection = ConnectionFactory.createConnection(this.configuration);
-            this.hBaseAdmin = (HBaseAdmin) this.connection.getAdmin();
+            connection = ConnectionFactory.createConnection(this.configuration, this.executorService);
+            this.hBaseAdmin = (HBaseAdmin) connection.getAdmin();
+            return connection;
         } catch (IOException e) {
             e.printStackTrace();
         }
+        return connection;
     }
 
     public static void main(String[] args) {
 
         HbaseTest hbaseTest = new HbaseTest();
-        hbaseTest.droptable();
-        hbaseTest.createtab();
-        hbaseTest.putdata();
-        hbaseTest.getdata();
+        // hbaseTest.droptable();
+        // hbaseTest.createtab();
+        // hbaseTest.putdata();
+        // hbaseTest.getdata();
+        // testpool(hbaseTest);
+        // getpagedata(hbaseTest);
+        counter(hbaseTest);
+    }
+
+    /**
+     *
+     * @Title: counter
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @author scc
+     * @date 2016年11月22日 上午11:47:53
+     * @param hbaseTest
+     */
+    private static void counter(HbaseTest hbaseTest) {
+        hbaseTest.counter();
+    }
+
+    /**
+     *
+     * @Title: counter
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @author scc
+     * @date 2016年11月22日 上午11:48:07
+     */
+    private void counter() {
+        try {
+            Table table = this.getConnection().getTable(TableName.valueOf("counters"));
+            System.out.println(table.incrementColumnValue("rk1".getBytes(), "cf1".getBytes(), "count".getBytes(), 1));
+            Result result = table.get(new Get("rk1".getBytes()));
+            for (Cell cell : result.listCells()) {
+                System.out
+                        .println(Bytes.toString(cell.getFamilyArray(), cell.getFamilyOffset(), cell.getFamilyLength()));
+                System.out.println(
+                        Bytes.toString(cell.getQualifierArray(), cell.getQualifierOffset(), cell.getQualifierLength()));
+                System.out.println(Bytes.toString(cell.getValueArray(), cell.getValueOffset(), cell.getValueLength()));
+            }
+            table.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     *
+     * @Title: getpagedata
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @author scc
+     * @date 2016年11月22日 上午11:13:08
+     * @param hbaseTest
+     */
+    private static void getpagedata(HbaseTest hbaseTest) {
+        hbaseTest.getpagedata();
+    }
+
+    /**
+     *
+     * @Title: getpagedata
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @author scc
+     * @date 2016年11月22日 上午11:13:54
+     */
+    private void getpagedata() {
+        try {
+            Table table = this.getConnection().getTable(TableName.valueOf("test_snappy2"));
+            PageFilter pageFilter = new PageFilter(10);
+            int totalrows = 0;
+            byte[] lastrow = "1479783482796".getBytes();
+            Scan scan = new Scan();
+            scan.setFilter(pageFilter);
+            /*
+             * if (null != lastrow) {
+             * byte[] startrow = lastrow;
+             * Bytes.toStringBinary(startrow);
+             * scan.setStartRow(startrow);
+             * }
+             */
+            ResultScanner scanner = table.getScanner(scan);
+            int localrows = 0;
+            Result result;
+            while ((result = scanner.next()) != null) {
+                System.out.println(localrows++ + ":" + result);
+                totalrows++;
+                lastrow = result.getRow();
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     *
+     * @Title: testpool
+     * @Description: TODO(这里用一句话描述这个方法的作用)
+     * @author scc
+     * @date 2016年11月22日 上午10:30:27
+     */
+    private static void testpool(final HbaseTest hbaseTest) {
+        for (int i = 0; i < 20; i++) {
+            new Thread(new Runnable() {
+                public void run() {
+                    hbaseTest.putdata();
+                }
+            }).start();
+        }
     }
 
     /**
@@ -82,11 +202,13 @@ public class HbaseTest {
      */
     private void getdata() {
         try {
-            Table table = this.connection.getTable(TableName.valueOf("test_snappy2"));
+            Table table = this.getConnection().getTable(TableName.valueOf("test_snappy2"));
             Get get = new Get("a".getBytes());
+            // byte[][] colkey = KeyValue.parseColumn(Bytes.toBytes("colfamly:val1"));
             Result result = table.get(get);
             if (null != result) {
-                for (Cell cell : result.rawCells()) {
+                List<Cell> data = result.listCells();
+                for (Cell cell : data) {
                     System.out.println("列簇为：" + new String(cell.getFamily()));
                     System.out.println("列修饰符为：" + new String(cell.getQualifier()));
                     System.out.println("值为：" + new String(cell.getValue()));
@@ -124,16 +246,39 @@ public class HbaseTest {
      * @Title: putdata
      * @Description: TODO(这里用一句话描述这个方法的作用)
      * @author scc
+     * @param i
      * @date 2016年11月21日 下午2:56:24
      */
     private void putdata() {
-        try {
-            Table table = this.connection.getTable(TableName.valueOf("test_snappy2"));
-            Put put = new Put("a".getBytes());
-            put.addColumn("colfam".getBytes(), "val1".getBytes(), "colfam1".getBytes());
-            table.put(put);
-        } catch (IOException e) {
-            e.printStackTrace();
+        for (int i = 0; i < 100; i++) {
+            long s = System.currentTimeMillis();
+            Connection connection = null;
+            Table table = null;
+            try {
+                connection = this.getConnection();
+                table = connection.getTable(TableName.valueOf("test_snappy2"));
+                Put put = new Put(Bytes.toBytes(System.currentTimeMillis() + ""));
+                put.addColumn("colfam".getBytes(), "val1".getBytes(), "colfam1".getBytes());
+                table.put(put);
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (null != table) {
+                        table.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    if (null != connection) {
+                        connection.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println(Thread.currentThread().getName() + ",cost:" + (System.currentTimeMillis() - s));
         }
     }
 

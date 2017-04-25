38488a60cddb0f18ade9a6a44e7cb5cb006fad98
scc <javascc@126.com>
2017-04-25 15:50:12 +0800

    修复启动bug

--- vdc_center/src/main/java/com/voole/cdcenter/controller/manager/system/notice/NoticeController.java
+++ vdc_center/src/main/java/com/voole/cdcenter/controller/manager/system/notice/NoticeController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpSession;
 
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;

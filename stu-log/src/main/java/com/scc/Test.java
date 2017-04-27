a6ecb54ba04c8ed0dad814853830e7203edf6b3a
scc <javascc@126.com>
2017-04-27 17:24:25 +0800

    初始导入

---
+++ stu-log/src/main/java/com/scc/Test.java
@@ -0,0 +1,34 @@
+package com.scc;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @ClassName: Test
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author shangchengcai@voole.com
+ * @date 2017年4月27日 下午5:15:15
+ * 
+ */
+public class Test {
+	static Logger logger = LoggerFactory.getLogger(Test.class);
+
+	/**
+	 * <p>
+	 * Title:
+	 * </p>
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @author shangchengcai@voole.com
+	 * @date 2017年4月27日 下午5:15:15
+	 */
+	public Test() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		logger.info("ssss");
+	}
+}

58ddb5081e0a79a587f45ba202b5f67fdc7e964f
scc <javascc@126.com>
2017-04-27 17:28:44 +0800

    切换日志

--- vdc_center/src/main/java/com/voole/cdcenter/controller/BaseController.java
+++ vdc_center/src/main/java/com/voole/cdcenter/controller/BaseController.java
@@ -9,7 +9,8 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpSession;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.propertyeditors.CustomDateEditor;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
@@ -26,47 +27,47 @@ import com.voole.cdcenter.vo.system.settings.SystemSettingsVo;
  * 
  */
 public class BaseController {
-    protected final Logger LOGGER = Logger.getLogger(this.getClass());
-    @Resource
-    private ServletContext servletContext;
-    @Resource
-    private ISystemSettingsService systemSettingsService;
+	protected final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+	@Resource
+	private ServletContext servletContext;
+	@Resource
+	private ISystemSettingsService systemSettingsService;
 
-    /**
-     * 
-     * @Title: initBinder
-     * @Description: 类型转换器
-     * @param binder 参数
-     * @throws ServletException
-     */
-    @InitBinder
-    protected void initBinder(WebDataBinder binder) throws ServletException {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        dateFormat.setLenient(false);
-        binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
-    }
+	/**
+	 * 
+	 * @Title: initBinder
+	 * @Description: 类型转换器
+	 * @param binder 参数
+	 * @throws ServletException
+	 */
+	@InitBinder
+	protected void initBinder(WebDataBinder binder) throws ServletException {
+		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		dateFormat.setLenient(false);
+		binder.registerCustomEditor(Date.class, new CustomDateEditor(dateFormat, true));
+	}
 
-    /**
-     * @Title: refreshCacheConfig
-     * @Description: TODO(这里用一句话描述这个方法的作用)
-     * @author shangchengcai@voole.com
-     * @date 2016年7月18日 下午5:46:14
-     */
-    protected void refreshCacheConfig(SystemSettingsEntry sse, HttpSession session) {
-        if (sse.getIs_on() == 0) {
-            session.getServletContext().removeAttribute(sse.getConfig_name());
-            return;
-        }
-        if (null != sse.getId()) {
-            SystemSettingsVo settingsquerycase = new SystemSettingsVo();
-            settingsquerycase.setId(sse.getId());
-            List<SystemSettingsVo> settingsVos = this.systemSettingsService.querySystenSettings(settingsquerycase);
-            if (null != settingsVos && settingsVos.size() == 1) {
-                session.getServletContext().setAttribute(settingsVos.get(0).getConfig_name(),
-                        settingsVos.get(0).getConfig_value());
-            }
-        } else {
-            session.getServletContext().setAttribute(sse.getConfig_name(), sse.getConfig_value());
-        }
-    }
+	/**
+	 * @Title: refreshCacheConfig
+	 * @Description: TODO(这里用一句话描述这个方法的作用)
+	 * @author shangchengcai@voole.com
+	 * @date 2016年7月18日 下午5:46:14
+	 */
+	protected void refreshCacheConfig(SystemSettingsEntry sse, HttpSession session) {
+		if (sse.getIs_on() == 0) {
+			session.getServletContext().removeAttribute(sse.getConfig_name());
+			return;
+		}
+		if (null != sse.getId()) {
+			SystemSettingsVo settingsquerycase = new SystemSettingsVo();
+			settingsquerycase.setId(sse.getId());
+			List<SystemSettingsVo> settingsVos = this.systemSettingsService.querySystenSettings(settingsquerycase);
+			if (null != settingsVos && settingsVos.size() == 1) {
+				session.getServletContext().setAttribute(settingsVos.get(0).getConfig_name(),
+						settingsVos.get(0).getConfig_value());
+			}
+		} else {
+			session.getServletContext().setAttribute(sse.getConfig_name(), sse.getConfig_value());
+		}
+	}
 }
